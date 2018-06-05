a93f4ef1e140ed42d0653933b51945a2a10c10a0
ajaybongani <ajayandrodev@gmail.com>
2018-06-05 18:41:24 +0530

    added the ansible history

--- AjDevops/app/src/main/java/com/ajayrockstarindevops/ajdevops/GoogleSigninActivity.kt
+++ AjDevops/app/src/main/java/com/ajayrockstarindevops/ajdevops/GoogleSigninActivity.kt
@@ -18,103 +18,106 @@ import com.google.firebase.auth.GoogleAuthProvider
 
 class GoogleSigninActivity : AppCompatActivity(), View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
 
-  private val TAG = "AJAYGoogleSignIn"
-  private val REQUEST_CODE_SIGN_IN = 1234
-  private val WEB_CLIENT_ID = "885498913691-ji39kjot48v9er73end7b0p98uu1h6s3.apps.googleusercontent.com"
+    private val TAG = "AJAYGoogleSignIn"
+    private val REQUEST_CODE_SIGN_IN = 1234
+    private val WEB_CLIENT_ID = "885498913691-ji39kjot48v9er73end7b0p98uu1h6s3.apps.googleusercontent.com"
 
-  private var mAuth: FirebaseAuth? = null
+    private var mAuth: FirebaseAuth? = null
 
-  private var mGoogleApiClient: GoogleApiClient? = null
+    private var mGoogleApiClient: GoogleApiClient? = null
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_google_signin)
-    btn_sign_in.setOnClickListener(this)
-    val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-      .requestIdToken(WEB_CLIENT_ID)
-      .requestEmail()
-      .build()
-    mGoogleApiClient = GoogleApiClient.Builder(this)
-      .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
-      .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-      .build()
-    mAuth = FirebaseAuth.getInstance()
-  }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_google_signin)
+        btn_sign_in.setOnClickListener(this)
 
-  override fun onStart() {
-    super.onStart()
-    // Check if user is signed in (non-null) and update UI accordingly.
-    val currentUser = mAuth!!.currentUser
-    updateUI(currentUser)
-  }
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(WEB_CLIENT_ID)
+                .requestEmail()
+                .build()
 
-  override fun onClick(v: View?) {
-    val i = v!!.id
-    when (i) {
-      R.id.btn_sign_in -> signIn()
+        mGoogleApiClient = GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build()
 
+        mAuth = FirebaseAuth.getInstance()
     }
-  }
 
-  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-    super.onActivityResult(requestCode, resultCode, data)
-    // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent();
-    if (requestCode == REQUEST_CODE_SIGN_IN) {
-      val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
-      if (result.isSuccess) {
-        // successful -> authenticate with Firebase
-        val account = result.signInAccount
-        firebaseAuthWithGoogle(account!!)
-      } else {
-        // failed -> update UI
-        updateUI(null)
-        Toast.makeText(applicationContext, "SignIn: failed!",
-          Toast.LENGTH_SHORT).show()
-      }
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = mAuth!!.currentUser
+        updateUI(currentUser)
     }
-  }
 
-  private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
-    Log.e(TAG, "firebaseAuthWithGoogle():" + acct.id!!)
-    val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
-    mAuth!!.signInWithCredential(credential)
-      .addOnCompleteListener(this) { task ->
-        if (task.isSuccessful) {
-          // Sign in success
-          Log.e(TAG, "signInWithCredential: Success!")
-          val user = mAuth!!.currentUser
-          updateUI(user)
-        } else {
-          // Sign in fails
-          Log.w(TAG, "signInWithCredential: Failed!", task.exception)
-          Toast.makeText(applicationContext, "Authentication failed!",
-            Toast.LENGTH_SHORT).show()
-          updateUI(null)
+    override fun onClick(v: View?) {
+        val i = v!!.id
+        when (i) {
+            R.id.btn_sign_in -> signIn()
+
         }
-      }
-  }
-
-  override fun onConnectionFailed(connectionResult: ConnectionResult) {
-    Log.e(TAG, "onConnectionFailed():" + connectionResult);
-    Toast.makeText(applicationContext, "Google Play Services error.", Toast.LENGTH_SHORT).show();
-  }
-
-  private fun signIn() {
-    val intent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient)
-    startActivityForResult(intent, REQUEST_CODE_SIGN_IN)
-  }
-
-  private fun updateUI(user: FirebaseUser?) {
-    if (user != null) {
-      val intent = Intent(this, NavigationDrawerActivity::class.java)
-      intent.putExtra("name", user.displayName)
-      intent.putExtra("email", user.email)
-      startActivity(intent)
-    } else {
-      /* tvStatus.text = "Signed Out"
-       tvDetail.text = null
-       btn_sign_in.visibility = View.VISIBLE
-       layout_sign_out_and_disconnect.visibility = View.GONE*/
     }
-  }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent();
+        if (requestCode == REQUEST_CODE_SIGN_IN) {
+            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if (result.isSuccess) {
+                // successful -> authenticate with Firebase
+                val account = result.signInAccount
+                firebaseAuthWithGoogle(account!!)
+            } else {
+                // failed -> update UI
+                updateUI(null)
+                Toast.makeText(applicationContext, "SignIn: failed!",
+                        Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        Log.e(TAG, "firebaseAuthWithGoogle():" + acct.id!!)
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        mAuth!!.signInWithCredential(credential)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success
+                        Log.e(TAG, "signInWithCredential: Success!")
+                        val user = mAuth!!.currentUser
+                        updateUI(user)
+                    } else {
+                        // Sign in fails
+                        Log.w(TAG, "signInWithCredential: Failed!", task.exception)
+                        Toast.makeText(applicationContext, "Authentication failed!",
+                                Toast.LENGTH_SHORT).show()
+                        updateUI(null)
+                    }
+                }
+    }
+
+    override fun onConnectionFailed(connectionResult: ConnectionResult) {
+        Log.e(TAG, "onConnectionFailed():" + connectionResult);
+        Toast.makeText(applicationContext, "Google Play Services error.", Toast.LENGTH_SHORT).show();
+    }
+
+    private fun signIn() {
+        val intent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient)
+        startActivityForResult(intent, REQUEST_CODE_SIGN_IN)
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        if (user != null) {
+            val intent = Intent(this, NavigationDrawerActivity::class.java)
+            intent.putExtra("name", user.displayName)
+            intent.putExtra("email", user.email)
+            startActivity(intent)
+        } else {
+            /* tvStatus.text = "Signed Out"
+             tvDetail.text = null
+             btn_sign_in.visibility = View.VISIBLE
+             layout_sign_out_and_disconnect.visibility = View.GONE*/
+        }
+    }
 }
