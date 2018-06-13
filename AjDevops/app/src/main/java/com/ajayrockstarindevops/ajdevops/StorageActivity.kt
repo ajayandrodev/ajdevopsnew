eeeda1530760b9ce76a25b27ce8d8c8bfa9841d8
ajaybongani <ajayandrodev@gmail.com>
2018-06-13 19:52:23 +0530

    file checking existed or not

--- AjDevops/app/src/main/java/com/ajayrockstarindevops/ajdevops/StorageActivity.kt
+++ AjDevops/app/src/main/java/com/ajayrockstarindevops/ajdevops/StorageActivity.kt
@@ -40,10 +40,7 @@ class StorageActivity : AppCompatActivity(), View.OnClickListener {
         imageReference = FirebaseStorage.getInstance().reference.child("images")
 
         btn_choose_file.setOnClickListener(this)
-        btn_upload_byte.setOnClickListener(this)
         btn_upload_file.setOnClickListener(this)
-        btn_upload_stream.setOnClickListener(this)
-        btn_back.setOnClickListener(this)
     }
 
     override fun onClick(view: View?) {
@@ -51,50 +48,10 @@ class StorageActivity : AppCompatActivity(), View.OnClickListener {
 
         when (i) {
             R.id.btn_choose_file -> showChoosingFile()
-            R.id.btn_upload_byte -> uploadBytes()
             R.id.btn_upload_file -> uploadFile()
-            R.id.btn_upload_stream -> uploadStream()
-            R.id.btn_back -> finish()
         }
     }
 
-    private fun uploadBytes() {
-        if (fileUri != null) {
-            val fileName = edtFileName.text.toString()
-
-            if (!validateInputFileName(fileName)) {
-                return
-            }
-
-            val baos = ByteArrayOutputStream()
-            bitmap!!.compress(Bitmap.CompressFormat.JPEG, 50, baos)
-            val data: ByteArray = baos.toByteArray()
-
-            val fileRef = imageReference!!.child(fileName + "." + getFileExtension(fileUri!!))
-            fileRef.putBytes(data)
-                    .addOnSuccessListener { taskSnapshot ->
-                        Log.e(TAG, "Uri: " + taskSnapshot.downloadUrl)
-                        Log.e(TAG, "Name: " + taskSnapshot.metadata!!.name)
-                        tvFileName.text = taskSnapshot.metadata!!.path + " - " + taskSnapshot.metadata!!.sizeBytes / 1024 + " KBs"
-                        Toast.makeText(this, "File Uploaded ", Toast.LENGTH_LONG).show()
-                    }
-                    .addOnFailureListener { exception ->
-                        Toast.makeText(this, exception.message, Toast.LENGTH_LONG).show()
-                    }
-                    .addOnProgressListener { taskSnapshot ->
-                        // progress percentage
-                        val progress = 100.0 * taskSnapshot.bytesTransferred / taskSnapshot.totalByteCount
-
-                        // percentage in progress dialog
-                        val intProgress = progress.toInt()
-                        tvFileName.text = "Uploaded " + intProgress + "%..."
-                    }
-                    .addOnPausedListener { System.out.println("Upload is paused!") }
-
-        } else {
-            Toast.makeText(this, "No File!", Toast.LENGTH_LONG).show()
-        }
-    }
 
     private fun uploadFile() {
         if (fileUri != null) {
@@ -130,42 +87,6 @@ class StorageActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-    private fun uploadStream() {
-        if (fileUri != null) {
-            val fileName = edtFileName.text.toString()
-
-            if (!validateInputFileName(fileName)) {
-                return
-            }
-
-            try {
-                val stream: InputStream = contentResolver.openInputStream(fileUri)
-
-                val fileRef = imageReference!!.child(fileName + "." + getFileExtension(fileUri!!))
-                fileRef.putStream(stream)
-                        .addOnSuccessListener { taskSnapshot ->
-                            Log.e(TAG, "Uri: " + taskSnapshot.downloadUrl)
-                            Log.e(TAG, "Name: " + taskSnapshot.metadata!!.name)
-                            tvFileName.text = taskSnapshot.metadata!!.path + " - " + taskSnapshot.metadata!!.sizeBytes / 1024 + " KBs"
-                            Toast.makeText(this, "File Uploaded ", Toast.LENGTH_LONG).show()
-                        }
-                        .addOnFailureListener { exception ->
-                            Toast.makeText(this, exception.message, Toast.LENGTH_LONG).show()
-                        }
-                        .addOnProgressListener { taskSnapshot ->
-                            // because this is a stream so:
-                            // taskSnapshot.getTotalByteCount() = -1 (always)
-                            tvFileName.text = "Uploaded " + taskSnapshot.bytesTransferred + " Bytes..."
-                        }
-                        .addOnPausedListener { System.out.println("Upload is paused!") }
-
-            } catch (e: FileNotFoundException) {
-                e.printStackTrace()
-            }
-        } else {
-            Toast.makeText(this, "No File!", Toast.LENGTH_LONG).show()
-        }
-    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -178,7 +99,7 @@ class StorageActivity : AppCompatActivity(), View.OnClickListener {
             fileUri = data.data
             try {
                 bitmap = MediaStore.Images.Media.getBitmap(contentResolver, fileUri)
-                imgFile.setImageBitmap(bitmap)
+              //  imgFile.setImageBitmap(bitmap)
             } catch (e: IOException) {
                 e.printStackTrace()
             }
@@ -202,6 +123,9 @@ class StorageActivity : AppCompatActivity(), View.OnClickListener {
     private fun validateInputFileName(fileName: String): Boolean {
         if (TextUtils.isEmpty(fileName)) {
             Toast.makeText(this, "Enter file name!", Toast.LENGTH_SHORT).show()
+            return false
+        }else if(fileName.equals(edtFileName.text.toString())){
+            Toast.makeText(this, "File is already exist!", Toast.LENGTH_SHORT).show()
             return false
         }
 
