11ff70625c72d729f4d182fb1835bbb741fd7503
ajay <ajay.bongani@cattechnologies.com>
2018-07-10 13:59:08 +0530

    added git interviewquesions

--- AjDevops/app/src/main/java/com/ajayrockstarindevops/interviewQuestionsTools/GitInterviewQuestionsFragment.kt
+++ AjDevops/app/src/main/java/com/ajayrockstarindevops/interviewQuestionsTools/GitInterviewQuestionsFragment.kt
@@ -3,11 +3,20 @@ package com.ajayrockstarindevops.interviewQuestionsTools
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import com.ajayrockstarindevops.adapter.GitAdapter.GitCommandsAdapter
+import com.ajayrockstarindevops.adapter.GitAdapter.GitInterviewAdapter
 
 import com.ajayrockstarindevops.ajdevops.R
+import com.ajayrockstarindevops.model.GitModel.GitCommandsModel
+import com.ajayrockstarindevops.model.GitModel.GitInterviewModel
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -36,7 +45,24 @@ class GitInterviewQuestionsFragment : Fragment() {
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                             savedInstanceState: Bundle?): View? {
     // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_git_interview_questions, container, false)
+    val view = inflater.inflate(R.layout.fragment_git_interview_questions, container, false)
+    //getting recyclerview from xml
+    val recyclerView = view.findViewById(R.id.recyclerView) as RecyclerView
+    //adding a layoutmanager
+    recyclerView.setHasFixedSize(true)
+    val itemDecor = DividerItemDecoration(context, LinearLayout.VERTICAL)
+    recyclerView.addItemDecoration(itemDecor)
+    recyclerView.itemAnimator = DefaultItemAnimator()
+    recyclerView.layoutManager = LinearLayoutManager(context, LinearLayout.VERTICAL, false)
+    //crating an arraylist to store users using the data class user
+    val users = ArrayList<GitInterviewModel>()
+
+    //creating our adapter
+    val adapter = GitInterviewAdapter(users)
+    //now adding the adapter to recyclerview
+    recyclerView.adapter = adapter
+    return view
+
   }
 
 
---
+++ AjDevops/app/src/main/java/com/ajayrockstarindevops/model/GitModel/GitInterviewModel.kt
@@ -0,0 +1,7 @@
+package com.ajayrockstarindevops.model.GitModel
+
+
+/**
+ * Created by Ajay on 7/10/2018.
+ */
+data class GitInterviewModel(val question: String, val answer: String)
